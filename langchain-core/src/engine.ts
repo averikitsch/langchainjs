9b343e643fefc3ff57b412d56489dcdc9c189cc9
Itzaprado <98774659+ItzayanaPr@users.noreply.github.com>
2024-11-24 07:09:07 +0000

    feat(test): adding engine main test

--- langchain-core/src/engine.ts
+++ langchain-core/src/engine.ts
@@ -1,37 +1,15 @@
-/* 
-The constructor receives:
-
-Args:
-  key (object): Prevent direct constructor usage.
-  pool (AsyncEngine): Async engine connection pool.
-  loop (Optional[asyncio.AbstractEventLoop]): Async event loop used to create the engine.
-  thread (Optional[Thread]): Thread used to create the engine async.
-  
-  Raises:
-    Exception: If the constructor is called directly by the user. 
-*/
-
-// The create method, creates the DB connection and returns an PostgresEngine instance
-
-import { CredentialBody, GoogleAuth } from 'google-auth-library';
-import { ADCResponse, JSONClient, } from 'google-auth-library/build/src/auth/googleauth';
-import { Connector, IpAddressTypes } from "@google-cloud/cloud-sql-connector";
+import { Connector, IpAddressTypes} from "@google-cloud/cloud-sql-connector";
+import { GoogleAuth } from "google-auth-library";
 import Knex from "knex";
 
 /**
  * Get email address associated with current authenticated IAM principal.
  * Email will be used for automatic IAM database authentication to Cloud SQL.
  * 
- * @param {GoogleAuth} credentials - object to use in finding the associated IAM principal email address.
+ * @param {GoogleAuth} auth - object to use in finding the associated IAM principal email address.
  * @returns {string} email - email address associated with the current authenticated IAM principal
  */
-const getIAMPrincipalEmail = async (auth: GoogleAuth<JSONClient>): Promise<string> => {
-  // TODO: Implement this function
-  // Python validation: 
-  // if(!auth.credential.credentials.access_token) { // How can I validate that the object has an token and if not how can I refresh it
-  //   const newCredentials = 
-  //   auth.credential.setCredentials(newCredentials)
-  // }
+const getIAMPrincipalEmail = async (auth: GoogleAuth): Promise<string> => {
   const credentials = await auth.getCredentials();
   if('client_email' in credentials && credentials.client_email !== undefined) {
     return credentials.client_email.replace(".gserviceaccount.com", "");
@@ -39,7 +17,7 @@ const getIAMPrincipalEmail = async (auth: GoogleAuth<JSONClient>): Promise<strin
 
   const accessToken = await auth.getAccessToken();
   const url = `https://oauth2.googleapis.com/tokeninfo?access_token=${accessToken}}`;
-  const clientResponse = await auth.request({url}).then(res => res.data)
+  const clientResponse = await auth.request({url}).then((res: { data: any; }) => res.data)
   
   if(!('email' in clientResponse)) {
     throw new Error(
@@ -54,24 +32,38 @@ const getIAMPrincipalEmail = async (auth: GoogleAuth<JSONClient>): Promise<strin
 class PostgresEngine {
 
   static _createKey = Symbol();
-  pool: Promise<Knex.Knex<any, unknown[]>>;
+  private pool: Knex.Knex<any, any[]>;
+  static connector: Connector;
 
-  constructor(key: Symbol, pool: Promise<Knex.Knex<any, unknown[]>>) {
+  constructor(key: Symbol, pool: Knex.Knex<any, any[]>) {
     if(key !== PostgresEngine._createKey) {
       throw new Error("Only create class through 'create' method!")
     }
     this.pool = pool;
   }
 
+  /**
+   * 
+   * @param projectId GCP Project ID
+   * @param region Postgres Instance Region
+   * @param instanceName Postgres Instance name
+   * @param dbName Database name
+   * @param ipType IP address type. Defaults to IPAddressType.Public
+   * @param user Optional - Postgres user name. Defaults to undefined
+   * @param password Optional - Postgres user password. Defaults to undefined
+   * @param iamAccountEmail Optional - IAM service account email. Defaults to undefined
+   * @returns PostgressEngine instance
+   */
+
   static async create(
       projectId: string,
       region: string,
       instanceName: string,
       dbName: string,
-      ipType: IpAddressTypes,
+      ipType: IpAddressTypes = IpAddressTypes.PUBLIC,
       user?: string, 
       password?: string,
-      iamAccountEmail?: string,       
+      iamAccountEmail?: string,
     ) {
 
     let dbUser: string;
@@ -82,7 +74,7 @@ class PostgresEngine {
         "both should be specified to use basic user/password " +
         "authentication or neither for IAM DB authentication."
       )
-    } 
+    }
 
     if(user !== "" && password !== "") { // user and password are given so we use the basic auth
       dbUser = user!;
@@ -91,7 +83,7 @@ class PostgresEngine {
         dbUser = iamAccountEmail
       } else {
         // Get application default credentials
-        const auth = await new GoogleAuth({
+        const auth = new GoogleAuth({
           scopes: "https://www.googleapis.com/auth/userinfo.email"
         })
         // dbUser should be the iam principal email by passing the credentials obtained
@@ -99,9 +91,9 @@ class PostgresEngine {
       }
     }
 
-    const getConnection = async () => {
-      const connector = new Connector()
-      const clientOpts = await connector.getOptions({
+    const getConnection = async (): Promise<Knex.Knex> => {
+      PostgresEngine.connector = new Connector();
+      const clientOpts = await PostgresEngine.connector.getOptions({
         instanceConnectionName: `${projectId}:${region}:${instanceName}`,
         ipType: ipType
       })
@@ -112,15 +104,38 @@ class PostgresEngine {
           user: dbUser,
           password: password, 
           database: dbName,
+        },
+        pool: {
+          min: 2,
+          max: 10, // How many connections should be the max connections,
+          acquireTimeoutMillis: 30000
         }
       };
       return Knex(dbConfig)
     }
 
-    const engine = getConnection()
+    const engine = await getConnection()
+      .then( response => response)
+      .catch(error => {
+        throw new Error(`Error while trying to establish connection ${error}`)
+      });
 
     return new PostgresEngine(PostgresEngine._createKey, engine)
+  }
 
-    // Then use the SQLAlchemy to create the postgress engine
+  /**
+   *  Dispose of connection pool
+   */
+  async closeConnection(): Promise<void> {
+    await this.pool.destroy()
+    PostgresEngine.connector.close();
+  }
+
+  // Just to test the connection to the database
+  testConnection () {
+    const now = this.pool.raw('SELECT NOW() as currentTimestamp')
+    return now;
   }
 }
+
+export default PostgresEngine;
